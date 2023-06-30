70bb7edf882ddee8c5ef078e6258369e7c178876
brittbruntink <49716360+brittbruntink@users.noreply.github.com>
2023-06-30 07:56:20 +0200

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 12 12:52:03 2023
+#!/usr/bin/env python
+# File name: payment.py
+# Authors: E. Lan, A. van Weerden, B.M. Bruntink 
+# This code compares actual routes with standard routes and calculates the payment amount for each driver based on the similarity between the routes. It then generates CSV files containing the payment information and calculates the amount to be deposited back to the bank with a penalty fee if there is remaining payment.
+# Date: 30-06-2023
 
-@author: britt
-"""
+import os
+import csv
 import json
 from difflib import SequenceMatcher
-
 
 # Function to calculate similarity ratio between two routes
 def calculate_similarity(route1, route2):
@@ -19,7 +20,6 @@ def calculate_similarity(route1, route2):
     merchandise_similarity = SequenceMatcher(None, list(merchandise1), list(merchandise2)).ratio()
 
     return (city_similarity + merchandise_similarity) / 2
-
 
 # Read actual routes from actual.json
 with open('actual.json', 'r') as file:
@@ -46,45 +46,74 @@ for actual_route in actual_routes:
     if standard_route_match is not None:
         route_mapping[actual_route['id']] = standard_route_match['id']
 
-    
-
-
 # Function to calculate payment based on similarity
 def calculate_payment(similarity):
     # Define the compensation range and the corresponding payment range
     payment = 1000 * similarity
-
     return payment
-
 
 # Calculate the total payment available for the drivers
 total_payment = 500000
 remaining_payment = total_payment
 
 # Calculate the payment for each driver
-driver_payments = {}
+driver_payments = []
 
-for actual_id, standard_id in route_mapping.items():
-    actual_route = next((route for route in actual_routes if route['id'] == actual_id), None)
-    standard_route = next((route for route in standard_routes if route['id'] == standard_id), None)
+# Sort the actual routes based on descending similarity to the standard route
+sorted_actual_routes = sorted(actual_routes, key=lambda route: calculate_similarity(route, standard_routes[0]), reverse=True)
 
-    if actual_route and standard_route:
-        similarity = calculate_similarity(actual_route, standard_route)
-        payment = calculate_payment(similarity)
+payment_counter = 1
+csv_directory = 'results'
+os.makedirs(csv_directory, exist_ok=True)
+csv_filename = f"{csv_directory}/actual_routes{payment_counter}.csv"
 
-        # Reduce the remaining payment and check if it's sufficient to pay the driver
-        if payment <= remaining_payment:
-            driver_payments[actual_id] = payment, standard_id
-            remaining_payment -= payment
+with open(csv_filename, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Actual Route ID', 'Standard Route ID', 'Payment Amount'])
 
-# # Print the driver payments
-for item in driver_payments:
-    print("Driver for Actual Route", item, "which was most similar to Standard Route", driver_payments[item][1], "receives", driver_payments[item][0], "euros.")
+    for actual_route in sorted_actual_routes:
+        actual_id = actual_route['id']
+        standard_id = route_mapping.get(actual_id)
 
+        if standard_id:
+            standard_route = next((route for route in standard_routes if route['id'] == standard_id), None)
+            similarity = calculate_similarity(actual_route, standard_route)
+            payment = calculate_payment(similarity)
+
+            # Reduce the remaining payment and check if it's sufficient to pay the driver
+            if payment <= remaining_payment:
+                driver_payments.append((actual_route, standard_route, payment))
+                writer.writerow([actual_id, standard_id, payment])
+                remaining_payment -= payment
+            else:
+                break  # Stop further processing if there is not enough payment left
+
+        if remaining_payment <= 0:
+            # Close the current file and prepare for the next file
+            file.close()
+
+            # Calculate the amount to be deposited back to the bank
+            deposit_amount = remaining_payment
+            penalty_fee = deposit_amount * 0.05  # Assuming penalty fee is 5% of the deposit amount
+
+            # Print the deposit amount and penalty fee
+            print(f"Amount to be deposited back to the bank: {deposit_amount} euros.")
+            print(f"Penalty fee: {penalty_fee} euros.")
+
+            payment_counter += 1
+            csv_filename = f"{csv_directory}/paymentfile{payment_counter}.csv"
+
+            with open(csv_filename, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['Actual Route ID', 'Standard Route ID', 'Payment Amount'])
+
+            remaining_payment = total_payment
 
 # Calculate the amount to be deposited back to the bank
 deposit_amount = remaining_payment
+penalty_fee = deposit_amount * 0.05  # Assuming penalty fee is 5% of the deposit amount
 
 # Print the deposit amount and penalty fee
 print(f"Amount to be deposited back to the bank: {deposit_amount} euros.")
+print(f"Penalty fee: {penalty_fee} euros.")
 
